4e0ac0d6307222c3df4462f6df825c647ff7d4ff
huzankang <huzankang@sinozo.com>
2023-03-16 19:48:12 +0800

    随机数测试

---
+++ compatibility/src/main/java/com/jayus/test/RandomTest.java
@@ -0,0 +1,14 @@
+package com.jayus.test;
+
+import java.util.Random;
+
+/**
+ * @author : h zk
+ * @date : 2023/3/16 14:28
+ * @description :
+ **/
+public class RandomTest {
+    public static void main(String[] args) {
+        System.out.println(new Random().nextInt(940) + 60);
+    }
+}
